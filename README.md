65bb61aa9b1c9d4993b16be2b1143f3a1d717bf2
Chris Mikelson <chris.mikelson@640labs.com>
2015-08-13 23:25:38 -0500

    bacon updates

--- README.md
+++ README.md
@@ -12,5 +12,13 @@ gem "lita-bacon-ipsum"
 
 ## Usage
 
-    User: lita give me bacon
+    User: lita give me bacon text
     Lita: Bacon ipsum dolor amet...
+
+    User: lita give me bacon image
+    Lita: ![600 by 200 Bacon](https://baconmockup.com/600/200/ "600 by 200 Bacon")
+
+    _(optional)_
+    User: lita give me bacon image 800 by 1000
+    Lita: ![800 by 1000 Bacon](https://baconmockup.com/800/1000/ "800 by 1000 Bacon")
+
--- lib/lita/handlers/bacon_ipsum.rb
+++ lib/lita/handlers/bacon_ipsum.rb
@@ -1,20 +1,37 @@
 module Lita
   module Handlers
     class BaconIpsum < Handler
-      route(/give me bacon/i, :bacon_ipsum, command: true, help: {
+
+      ## Bacon Text
+      route(/give me bacon text/i, :bacon_ipsum_text, command: true, help: {
           "give me bacon" => "Bacon ipsum dolor amet..."
         })
 
-      def bacon_ipsum(response)
+      def bacon_ipsum_text(response)
         resp = http.get "https://baconipsum.com/api/?type=all-meat&start-with-lorem=1"
+
+        raise 'BaconFail' unless resp.status == 200
         
         bacon = MultiJson.load(resp.body)
 
-        if bacon
-          response.reply bacon[0]
-        else
-          response.reply "Sorry, I was unable to retreive bacon for you."
-        end
+        response.reply ">>>#{bacon[0]}"
+      rescue  
+        response.reply "Sorry, I was unable to retreive bacon for you."
+      end
+
+      ## Bacon Images
+      route(/give me bacon image ((?<width>\d+) by (?<height>\d+))?/i, :bacon_ipsum_image, command: true, help: {
+          "give me bacon image WIDTH by HEIGHT" => "Gives bacon image based on WIDTH and HEIGHT"
+        })
+
+      def bacon_ipsum_image(response)
+        random = rand((100...1000).step(100))
+        height = response.match_data[:height] ||= random
+        width  = response.match_data[:width] ||= random
+
+        response.reply "<http://baconmockup.com/#{width}/#{height}|#{width} by #{height} Bacon>"
+      rescue  
+        response.reply "Sorry, I was unable to retreive bacon for you."
       end
 
     end
